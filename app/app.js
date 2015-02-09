278b7c0f202c23fb96bd5c34ffd1c371ced9e5e6
elmeryang <elmerdanny.yang@gmail.com>
2015-02-08 23:26:12 -0800

    First Commit

--- app/app.js
+++ app/app.js
@@ -1,12 +1,64 @@
 'use strict';
 
 // Declare app level module which depends on views, and components
-angular.module('myApp', [
-  'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+angular.module('myModule', ['schemaForm'])
+       .controller('FormController', function($scope) {
+  $scope.schema = {
+      "type": "object",
+	  "title": "Comment",
+	  "required": [
+	    "comments"
+	  ],
+	  "properties": {
+	    "comments": {
+	      "type": "array",
+	      "maxItems": 2,
+	      "items": {
+	        "type": "object",
+	        "properties": {
+	          "name": {
+	            "title": "Name",
+	            "type": "string"
+	          },
+	          "email": {
+	            "title": "Email",
+	            "type": "string",
+	            "pattern": "^\\S+@\\S+$",
+	            "description": "Email will be used for evil."
+	          },
+	          "spam": {
+	            "title": "Spam",
+	            "type": "boolean",
+	            "default": true
+	          },
+	          "comment": {
+	            "title": "Comment",
+	            "type": "string",
+	            "maxLength": 20,
+	            "validationMessage": "Don't be greedy!"
+	          }
+	        },
+	        "required": [
+	          "name",
+	          "comment"
+	        ]
+	      }
+	    }
+	  }
+  };
+
+  $scope.form = [
+       "*",
+    {
+      type: "submit",
+      title: "Save"
+    }
+  ];
+
+  $scope.model = {
+   "comments": [
+    {
+      "spam": true
+    }
+  ]};
+});
